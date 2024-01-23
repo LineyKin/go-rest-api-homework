521e0b87968499184b76538766097f532331d3a7
Sam <yablokov-s-a@mail.ru>
2024-01-23 17:56:55 +0300

    добавлен обработчик для получения всех задач

--- precode.go
+++ precode.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -39,13 +40,36 @@ var tasks = map[string]Task{
 	},
 }
 
-// Ниже напишите обработчики для каждого эндпоинта
-// ...
+// Обработчик для получения всех задач
+func getTastks(w http.ResponseWriter, r *http.Request) {
+
+	// сериализуем данные из мапы tasks
+	resp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// записываем тип контента в заголовок
+	w.Header().Set("Content-Type", "application/json")
+
+	// записываем статус 200 OK
+	w.WriteHeader(http.StatusOK)
+
+	// записываем сам контент, а именно сериализованый json объект
+	w.Write(resp)
+
+}
 
 func main() {
 	r := chi.NewRouter()
 
+	// регистрируем в роутере эндпоинт `/tasks` с методом GET, для которого используется обработчик `getTastks`
+	// http://localhost:8080/tasks
+	r.Get("/tasks", getTastks)
+
 	// здесь регистрируйте ваши обработчики
+
 	// ...
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
